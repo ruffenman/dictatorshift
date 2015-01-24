d10fde80fbc9a3f531bc5c1e6bc3a39814dda14a
Samuel Holley <samuelholley@gmail.com>
2015-01-24 09:10:18 -0500

    Base class for CombinedPlayer and BodyPart.

---
+++ project/Assets/IGJam/Scripts/BodyPart.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BodyPart
+{
+	public enum BodyPartType
+	{
+		HEAD,
+		BODY,
+		ARMS,
+		LEGS
+	}
+
+	BodyPartType bodyPartType;
+	int playerIndex;
+
+	public BodyPart(BodyPartType newBodyPartType, int newPlayerIndex)
+	{
+		bodyPartType = newBodyPartType;
+		playerIndex = newPlayerIndex;
+	}
+
+	// Update is called once per frame
+	public void Update () 
+	{
+		Debug.Log(bodyPartType);
+	}
+}
---
+++ project/Assets/IGJam/Scripts/CombinedPlayer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CombinedPlayer : MonoBehaviour 
+{
+	BodyPart[] bodyParts;
+
+	// Use this for initialization
+	void Start () 
+	{
+		bodyParts = new BodyPart[4];
+		bodyParts[0] = new BodyPart(BodyPart.BodyPartType.HEAD, 0);
+		bodyParts[1] = new BodyPart(BodyPart.BodyPartType.BODY, 1);
+		bodyParts[2] = new BodyPart(BodyPart.BodyPartType.ARMS, 2);
+		bodyParts[3] = new BodyPart(BodyPart.BodyPartType.LEGS, 3);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		foreach (BodyPart bodyPart in bodyParts)
+		{
+			bodyPart.Update();
+		}	
+	}
+}
