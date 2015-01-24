358e676bd7fa400349d4d7152778d1aaa8eb0995
Carlo Mixco <mixco1a0@gmail.com>
2015-01-24 16:18:21 -0500

    lazer change, hook up input.

--- project/Assets/IGJam/Scripts/Lazer.cs
+++ project/Assets/IGJam/Scripts/Lazer.cs
@@ -14,14 +14,16 @@ public class Lazer : MonoBehaviour
     private PowerLevel mPowerLevel = PowerLevel.None;
     private IGJInputManager.InputDirection mDirection;
 
+    private Transform mPivot = null;
     private Transform mMiddleSection = null;
     private Transform mEndSection = null;
 
 	// Use this for initialization
 	void Start ()
     {
-        mMiddleSection = transform.FindChild("Pivot").FindChild("Middle");
-        mEndSection = transform.FindChild("Pivot").FindChild("End");
+        mPivot = transform.FindChild("Pivot");
+        mMiddleSection = mPivot.FindChild("Middle");
+        mEndSection = mPivot.FindChild("End");
 	}
 	
 	// Update is called once per frame
@@ -52,5 +54,37 @@ public class Lazer : MonoBehaviour
         {
             transform.localScale += new Vector3(0.0f, 2.5f, 0.0f);
         }
+
+        float angle = 0.0f;
+        switch (direction)
+        {
+            case IGJInputManager.InputDirection.UpRight:
+                angle = 45.0f;
+                break;
+            case IGJInputManager.InputDirection.Up:
+                angle = 90.0f;
+                break;
+            case IGJInputManager.InputDirection.UpLeft:
+                angle = 135.0f;
+                break;
+            case IGJInputManager.InputDirection.Left:
+                angle = 180.0f;
+                break;
+            case IGJInputManager.InputDirection.DownLeft:
+                angle = 225.0f;
+                break;
+            case IGJInputManager.InputDirection.Down:
+                angle = 270.0f;
+                break;
+            case IGJInputManager.InputDirection.DownRight:
+                angle = 315.0f;
+                break;
+            case IGJInputManager.InputDirection.Right:
+                // fallthrough
+            default:
+                // do nothing
+                break;
+        }
+        mPivot.Rotate(Vector3.forward, angle);
     }
 }
